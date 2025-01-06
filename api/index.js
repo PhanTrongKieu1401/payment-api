aaaaad69887684819cc72c7e46e60b96906b1ee8
PhanTrongKieu <PhanTrongKieu1401@users.noreply.github.com>
2025-01-06 23:54:06 +0700

    Add limit memory cache

--- api/index.js
+++ api/index.js
@@ -9,7 +9,8 @@ const cors = require("cors");
 const app = express();
 const port = 3000;
 app.use(cors());
-app.use(express.json());
+// app.use(express.json());
+app.use(express.json({ limit: '10mb' }));
 app.use(express.urlencoded());
 
 const MomoRequest = {
